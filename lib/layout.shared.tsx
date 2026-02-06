25e28b95ed5241796b8391ba1c0d4119b86e2b86
SKB <shubhambhilare4899223@gmail.com>
2026-02-06 21:44:44 +0530

    minor ui changes

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -1,10 +1,19 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Logo from "@/components/home/logo";
+import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      title: 'FolioKit',
+      title: <Logo size="sm"/>,
     },
-    githubUrl: 'https://github.com/skb3611/foliokit',
+
+    githubUrl: "https://github.com/skb3611/foliokit",
+    themeSwitch: {
+      component: <div className="w-full flex justify-end">
+      <AnimatedThemeToggler />
+      </div>,
+      mode: "light-dark",
+    },
   };
 }
