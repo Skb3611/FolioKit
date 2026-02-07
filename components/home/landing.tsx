4e888f759cc7949d97e6943f8d8584483e984ea6
SKB <shubhambhilare4899223@gmail.com>
2026-02-07 20:58:56 +0530

    added image section and stack section

--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./navbar";
 import Hero from "./hero";
 import Footer from "./footer";
+import ImageSection from "./images-section";
 const Landing = () => {
   return (
     <section className="max-w-6xl mx-auto py-20 min-h-screen">
